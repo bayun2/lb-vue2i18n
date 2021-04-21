35d2df3d4dd478cc3a76eef824cd82c358e1d576
“阮文涛” <wentao.ruan@longbridge.sg>
2021-04-21 16:28:13 +0800

    feat: 新增 繁体比对，下载多语言文件功能

--- README.md
+++ README.md
@@ -5,3 +5,13 @@
 通过 diff 命令可以比对 {rootProject}/lang/zh-CN.json 比 {rootProject}/lang/en.json 新增了哪些词条
 
 ![demo](https://cdn-support.lbkrs.com/uploads/files/201912/CkYBSKTn1pmcScE3mueYCFCcKCWCiKCc.gif)
+
+下载项目多语言文件，支持 loco 翻译平台指定 project 下载到当前项目
+可以在项目根目录的 package.json 中设置
+
+```json
+"lbVue2i18n": {
+  "locoExportKey": "xxxx", // 对应 loco 上的项目 api key
+  "langPath": "src/lang" // 默认 /lang
+}
+```
--- extension.js
+++ extension.js
@@ -1,9 +1,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode')
-const path = require('path')
-const { generate } = require('./lib/generate')
-const { diff } = require('./lib/diff')
+const vscode = require('vscode');
+const path = require('path');
+const { generate } = require('./lib/generate');
+const { exportFiles } = require('./lib/exportFiles');
+const { diffCNWithEN, diffCNWithHK } = require('./lib/diffCMD');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -13,74 +14,70 @@ const { diff } = require('./lib/diff')
 function activate(context) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "lb-vue2i18n" is now active!')
+  console.log('Congratulations, your extension "lb-vue2i18n" is now active!');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
     'extension.lbVue2i18n',
-    function() {
+    function () {
       // The code you place here will be executed every time your command is executed
       // /Users/rwt/gitlab/stock-activity/pages/index.vue
       const currentlyOpenTabfilePath =
-        vscode.window.activeTextEditor.document.fileName
+        vscode.window.activeTextEditor.document.fileName;
       if (
-        !path
-          .extname(currentlyOpenTabfilePath)
-          .toLowerCase()
-          .includes('vue')
+        !path.extname(currentlyOpenTabfilePath).toLowerCase().includes('vue')
       ) {
-        vscode.window.showInformationMessage('只能提取vue文件')
-        return false
+        vscode.window.showInformationMessage('只能提取vue文件');
+        return false;
       }
       // /Users/rwt/gitlab/stock-activity
-      const rootPath = vscode.workspace.workspaceFolders[0].uri.path
-      const hasReplaced = generate(currentlyOpenTabfilePath, rootPath)
-      const msg = hasReplaced
-        ? '成功提取中文到{projectRoot}/lang/zh-CN.json内'
-        : '没有需要提取的内容'
-      vscode.window.showInformationMessage(msg)
+      const rootPath = vscode.workspace.workspaceFolders[0].uri.path;
+      generate(
+        currentlyOpenTabfilePath,
+        rootPath,
+        vscode.window.showInformationMessage
+      );
     }
-  )
+  );
 
   let disposable2 = vscode.commands.registerCommand(
     'extension.lbDiffCNWithEN',
-    function() {
-      // /Users/rwt/gitlab/stock-activity
-      const rootPath = vscode.workspace.workspaceFolders[0].uri.path
-      const cnObj = require(path.join(rootPath, 'lang/zh-CN.json'))
-      const enObj = require(path.join(rootPath, 'lang/en.json'))
-      const diffObj = diff(cnObj, enObj)
-      const newFile = vscode.Uri.parse(
-        'untitled:' + path.join(rootPath, 'lang/diff.json')
-      )
-      vscode.workspace.openTextDocument(newFile).then(document => {
-        const edit = new vscode.WorkspaceEdit()
-        edit.insert(
-          newFile,
-          new vscode.Position(0, 0),
-          JSON.stringify(diffObj, null, '\t')
-        )
-        return vscode.workspace.applyEdit(edit).then(success => {
-          if (success) {
-            vscode.window.showTextDocument(document)
-          } else {
-            vscode.window.showInformationMessage('Error!')
-          }
-        })
-      })
+    function () {
+      diffCNWithEN();
     }
-  )
+  );
 
-  context.subscriptions.concat([disposable, disposable2])
+  let disposable3 = vscode.commands.registerCommand(
+    'extension.lbDiffCNWithHK',
+    function () {
+      diffCNWithHK();
+    }
+  );
+
+  let disposable4 = vscode.commands.registerCommand(
+    'extension.lbExportArchive',
+    function () {
+      // /Users/rwt/gitlab/stock-activity
+      const rootPath = vscode.workspace.workspaceFolders[0].uri.path;
+
+      exportFiles(rootPath, vscode.window.showInformationMessage);
+    }
+  );
+
+  context.subscriptions.concat([
+    disposable,
+    disposable2,
+    disposable3,
+    disposable4,
+  ]);
 }
-exports.activate = activate
 
 // this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
   activate,
-  deactivate
-}
+  deactivate,
+};
